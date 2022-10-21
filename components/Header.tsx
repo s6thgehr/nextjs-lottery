1de753d998499c45a71f208b56294fd677592cc9
Thomas Gehrmann <s6thgehr@gmail.com>
2022-10-21 11:52:29 +0200

    Write LotteryEntrance component

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@ import { ConnectButton } from "web3uikit";
 export default function Header() {
     return (
         <div>
+            Smart Contract Lottery
             <ConnectButton moralisAuth={false} />
         </div>
     );
---
+++ components/LotteryEntrance.tsx
@@ -0,0 +1,6 @@
+import { useWeb3Contract } from "react-moralis";
+import { abi, contractAddresses } from "../constants";
+
+export default function LotteryEntrance() {
+    return <div>Hello from entrance</div>;
+}
